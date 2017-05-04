1c3f8890127d3db1bcbb0ed9b565cc9334eea28a
Andres Pineda <humb.amp123@gmail.com>
2017-05-04 15:15:23 -0700

    Add print filter

--- src/ft_print_handler.c
+++ src/ft_print_handler.c
@@ -6,13 +6,107 @@
 /*   By: apineda <apineda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 14:07:30 by apineda           #+#    #+#             */
-/*   Updated: 2017/05/04 14:23:28 by apineda          ###   ########.fr       */
+/*   Updated: 2017/05/04 15:14:03 by apineda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_db.h"
 
-void	ft_print_handler(char **rec, t_table *t)
+
+static	int	ft_search_header(t_table *t, char *find)
+{
+	t_content	*data;
+	t_column	*col;
+	int			col_idx;
+	char		*str;
+	int			len;
+
+	col_idx = 0;
+	while (col_idx < g_col_id)
+	{
+		col = &t->columns[col_idx];
+		str = col->name;
+		if (str)
+			if(ft_strcmp(str, find))
+				return (col_idx);
+		col_idx++;
+	}
+	return (-1);
+}
+
+static	int	ft_search_col(t_table *t, int row_idx, char *find)
+{
+	t_content	*data;
+	t_column	*col;
+	int			col_idx;
+	char		*str;
+	int			ret;
+
+	col_idx = 0;
+	ret = 0;
+	while (col_idx < g_col_id)
+	{
+		col = &t->columns[col_idx];
+		data = &col->content_array[row_idx];
+		str = data->data;
+		if (ft_strstr(str, find))
+		{
+			ret = 1;
+		}
+		col_idx++;
+	}
+	return (ret);
+}
+
+static	int	ft_search_row(t_table *t, char *str)
+{	
+
+	int			row_idx;
+	int			len;
+	int			ret;
+
+	ft_print_header(t);
+	row_idx = 0;
+	while (row_idx < g_row_id)
+	{
+		if(t->row_id[row_idx] > 0)
+		{
+			if (ft_search_col(t, row_idx, str))
+			{
+				ft_print_row(t, row_idx);
+				ret = 1;
+			}
+		}
+		row_idx++;
+	}
+	ft_print_line(t);
+	return (ret);
+}
+
+void			ft_print_filtered(t_table *t)
+{
+	char	*str;
+	int		col_idx;
+
+	ft_print_header(t);
+	str = ft_get_info("What column would you like to filter");
+	if (str)
+	{
+		col_idx = ft_search_header(t, str);
+		if (col_idx >= 0)
+		{
+			str = ft_get_info("What would you like to filter for?");
+			if (str)
+			{
+				if (ft_search_row(t, str) == -1)
+					printf("Search of %s was not found.\n", str);
+			}
+		}
+	}
+
+}
+
+void			ft_print_handler(char **rec, t_table *t)
 {
 	int		row_num;
 	int		col_num;
@@ -37,8 +131,7 @@ void	ft_print_handler(char **rec, t_table *t)
 
 	if (!rec[1])
 		printf("print PRINT USAGE\n");
-
-	if (rec[1][0] == 'r')
+	else if (rec[1][0] == 'r')
 	{
 		if ((row_num = ft_atoi(ft_get_info("What row number?")) >= 0))
 			ft_print_row(t, row_num);
@@ -48,5 +141,5 @@ void	ft_print_handler(char **rec, t_table *t)
 	else if (rec[1][0] == 'c')
 		ft_print_header(t);
 	else if (rec[1][0] == 's')
-		printf("print sorted?\n");
+		ft_print_filtered(t);
 }

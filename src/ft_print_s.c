157d9822c4d06fe8780e28750ccbd62f42ced3c4
Andres Pineda <humb.amp123@gmail.com>
2017-05-04 23:33:10 -0700

    fix print selected rows

--- src/ft_print_s.c
+++ src/ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: apineda <apineda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 22:01:15 by apineda           #+#    #+#             */
-/*   Updated: 2017/05/04 23:01:48 by apineda          ###   ########.fr       */
+/*   Updated: 2017/05/04 23:32:26 by apineda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,17 @@
 
 void		ft_print_selected(t_table *t, int num1, int num2)
 {
-	printf("HERE\n");
-	if (num1 && num2 && num2 > num1)
+	if (num1 >= 0 && num2 >= 0 && num1 < num2)
 	{
 		ft_print_header(t);
 		while (num1 < num2)
 			ft_print_row(t, num1++);
 		ft_print_line(t);
 	}
-	else if (num1 && !num2)
+	else if (num1 >= 0 && num2 < 0)
 	{
 		ft_print_header(t);
-		ft_print_row(t, num1++);
+		ft_print_row(t, num1);
 		ft_print_line(t);
 	}
 	else

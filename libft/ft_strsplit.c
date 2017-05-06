731391d4b03f134afb1b0c76fe84238ad51d4fde
Queenie Ho <kuini.ho@gmail.com>
2017-05-05 18:47:48 -0700

    can load csv

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,186 +6,71 @@
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/12 10:28:15 by apineda           #+#    #+#             */
-/*   Updated: 2017/05/05 15:10:31 by qho              ###   ########.fr       */
+/*   Updated: 2017/05/05 18:47:10 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// static int	ft_whitespace(char const *s, char c, int i)
-// {
-// 	while (s[i] == c && s[i] != '\0')
-// 		i++;
-// 	return (i);
-// }
-
-// static int	ft_c(char const *s, char c, int i)
-// {
-// 	int		j;
-
-// 	j = 1;
-// 	while (s[i] != c && s[i] != '\0')
-// 	{
-// 		i++;
-// 		j++;
-// 	}
-// 	return (j);
-// }
-
-// static int	ft_words(char const *s, char c, int i)
-// {
-// 	int		j;
-
-// 	j = 1;
-// 	while (s[i] != '\0')
-// 	{
-// 		while (s[i] != c && s[i] != '\0')
-// 			i++;
-// 		j++;
-// 		i = ft_whitespace(s, c, i);
-// 	}
-// 	return (j);
-// }
-
-// char		**ft_strsplit(char const *s, char c)
-// {
-// 	char	**str;
-// 	int		i[3];
-
-// 	str = NULL;
-// 	ft_bzero((void*)i, sizeof(int) * 3);
-// 	if (s && c)
-// 	{
-// 		i[0] = ft_whitespace(s, c, i[0]);
-// 		if ((str = (char **)ft_memalloc(sizeof(char*) * ft_words(s, c, i[0]))))
-// 		{
-// 			while (s[i[0]] != '\0')
-// 			{
-// 				i[1] = 0;
-// 				str[i[2]] = (char *)ft_strnew(sizeof(char) * ft_c(s, c, i[0]));
-// 				while (s[i[0]] != c && s[i[0]] != '\0')
-// 					str[i[2]][i[1]++] = s[i[0]++];
-// 				str[i[2]][i[1]] = '\0';
-// 				i[2]++;
-// 				i[0] = ft_whitespace(s, c, i[0]);
-// 			}
-// 			str[i[2]] = NULL;
-// 		}
-// 	}
-// 	return (str);
-// }
-
-static int		ft_wrdcnt(char const *s, char c)
+static int	ft_whitespace(char const *s, char c, int i)
 {
-	int	cnt;
-	int	i;
-
-	cnt = 0;
-	i = 0;
-	while (s[i])
-	{
-		if ((i == 0 && s[i] != c) || (s[i] != c && s[i - 1] == c))
-			cnt++;
+	while (s[i] == c && s[i] != '\0')
 		i++;
-	}
-	return (cnt);
+	return (i);
 }
 
-
-static int	ft_location(int loc, char const *s, char c)
+static int	ft_c(char const *s, char c, int i)
 {
-	int	i;
+	int		j;
 
-	i = loc;
-	while (s[i])
+	j = 1;
+	while (s[i] != c && s[i] != '\0')
 	{
-		if (s[i] != c)
-		{
-			loc = i;
-			break ;
-		}
 		i++;
-	}
-	return (loc);
-}
-
-static int	ft_wordlen(int loc, char const *s, char c)
-{
-	int len;
-
-	len = 0;
-	while (s[loc])
-	{
-		if (s[loc] == c)
-			break ;
-		len++;
-		loc++;
-	}
-	return (len);
-}
-
-// static char	**ft_getwords(char const *s, char **words, char c)
-// {
-// 	int location;
-// 	int wlen;
-// 	int j;
-// 	int l;
-// 	char **ret;
-
-// 	location = 0;
-// 	j = 0;
-// 	ret = words;
-// 	while (j < ft_wrdcnt(s, c))
-// 	{
-// 		l = 0;
-// 		location = ft_location(location, s, c);
-// 		wlen = ft_wordlen(location, s, c);
-// 		ret[j] = (char *)malloc(sizeof(char) * (wlen + 1));
-// 		while (l < wlen)
-// 			ret[j][l++] = s[location++];
-// 		ret[j][l] = '\0';
-// 		j++;
-// 	}
-// 	ret[j] = NULL;
-// 	return (ret);
-// }
-
-static char	**ft_getwords(char const *s, char c, int len)
-{
-	int location;
-	int wlen;
-	int j;
-	int l;
-	char **ret;
-
-	location = 0;
-	j = 0;
-	ret = (char **)malloc(sizeof(char *) * len);
-	while (j < ft_wrdcnt(s, c))
-	{
-		l = 0;
-		location = ft_location(location, s, c);
-		wlen = ft_wordlen(location, s, c);
-		ret[j] = (char *)malloc(sizeof(char) * (wlen + 1));
-		while (l < wlen)
-			ret[j][l++] = s[location++];
-		ret[j][l] = '\0';
 		j++;
 	}
-	ret[j] = NULL;
-	return (ret);
+	return (j);
+}
+
+static int	ft_words(char const *s, char c, int i)
+{
+	int		j;
+
+	j = 1;
+	while (s[i] != '\0')
+	{
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		j++;
+		i = ft_whitespace(s, c, i);
+	}
+	return (j);
 }
 
 char		**ft_strsplit(char const *s, char c)
 {
-	char	**words;
+	char	**str;
+	int		i[3];
 
-	words = NULL;
+	str = NULL;
+	ft_bzero((void*)i, sizeof(int) * 3);
 	if (s && c)
-		words = ft_getwords(s, c, (ft_wrdcnt(s, c) + 1));
-		// words = (char **)malloc(sizeof(char *) * (ft_wrdcnt(s, c) + 1));
-	// if (words)
-	// 	words = ft_getwords(s, c, (ft_wrdcnt(s, c) + 1));
-	return (words);
+	{
+		i[0] = ft_whitespace(s, c, i[0]);
+		if ((str = (char **)ft_memalloc(sizeof(char*) * ft_words(s, c, i[0]))))
+		{
+			while (s[i[0]] != '\0')
+			{
+				i[1] = 0;
+				str[i[2]] = (char *)ft_strnew(sizeof(char) * ft_c(s, c, i[0]));
+				while (s[i[0]] != c && s[i[0]] != '\0')
+					str[i[2]][i[1]++] = s[i[0]++];
+				str[i[2]][i[1]] = '\0';
+				i[2]++;
+				i[0] = ft_whitespace(s, c, i[0]);
+			}
+			str[i[2]] = NULL;
+		}
+	}
+	return (str);
 }
-
--- src/ft_load_db.c
+++ src/ft_load_db.c
@@ -6,7 +6,7 @@
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 13:02:16 by qho               #+#    #+#             */
-/*   Updated: 2017/05/05 16:05:36 by qho              ###   ########.fr       */
+/*   Updated: 2017/05/05 18:47:34 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,53 +25,126 @@ void	ft_load_column(char *header, t_table *t)
 	t->col_id[idx] = ft_col_id_gen();
 	col = &(t->columns[idx]);
 	if (len == 1)
+	{
 		col->type = 's';
+		col->name = strdup(info[0]);
+	}
 	else
 	{
 		col->type = info[0][0];
-		// free(info[0]);
+		col->name = strdup(info[1]);
 	}
-	col->name = strdup(info[1]);
 	col->name_len = strlen(col->name);
 	col->max_len = col->name_len;
 	t->max_size += col->max_len;
 }
 
+int		ft_get_col_num(char *line)
+{
+	int cnt;
+	int	i;
+
+	cnt = 0;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == ',')
+			cnt++;
+		i++;
+	}
+	if (line[i - 1] != ',')
+		cnt++;
+	return (cnt);
+}
+
 void	ft_get_columns(char *line, t_table *t)
 {
-	char	**headers;
+	// char	**headers;
+	char	*header;
+	int		c_num;
+	char	*tmp;
 
-	headers = ft_strsplit(line, ',');
-	while (*headers)
+	// headers = ft_strsplit(line, ',');
+	c_num = ft_get_col_num(line);
+	printf("col numbers %d\n", c_num);
+	// while (*headers)
+	// {
+	// 	ft_load_column(*headers, t);
+	// 	headers++;
+	// }
+
+	while (c_num)
 	{
-		ft_load_column(*headers, t);
-		headers++;
+		if ((tmp = strchr(line, ',')))
+		{
+			*tmp = '\0';
+			tmp++;
+		}
+		if (!*line)
+			header = strdup("-");
+		else
+			header = strdup(line);
+		// printf("where?\n");
+		ft_load_column(header, t);
+		line = tmp;
+		c_num--;
 	}
+
 }
+
+
+// while (c_idx < COL_SIZE && t->col_id[c_idx])
+// 	{
+// 		if ((tmp = strchr(line, ',')))
+// 		{
+// 			*tmp = '\0';
+// 			tmp++;
+// 		}
+// 		if (!*line)
+// 			record = strdup("-");
+// 		else
+// 			record = strdup(line);
+// 		ft_load_row_data(t, r_idx, record, c_idx);
+// 		line = tmp;
+// 		c_idx++;
+// 	}
 
 void	ft_load_row_data(t_table *t, int r_idx, char *record, int c_idx)
 {
 	t_content	*content;
 
 	content = &t->columns[c_idx].content_array[r_idx];
-	content->data = record;
+	if (record)
+		content->data = record;
+	else
+		content->data = strdup("-");
 	content->len = strlen(content->data);
 	ft_update_maxlen(&t->columns[c_idx], content->len, &t->max_size);
 }
 
 void	ft_load_row(char *line, t_table *t)
 {
-	char	**records;
+	char	*record;
 	int		r_idx;
 	int		c_idx;
+	char	*tmp;
 
 	c_idx = 0;
 	r_idx = ft_empty_row(t);
 	t->row_id[r_idx] = ft_row_id_gen();
-	records = ft_strsplit(line, ',');
 	while (c_idx < COL_SIZE && t->col_id[c_idx])
 	{
-		ft_load_row_data(t, r_idx, records[c_idx], c_idx);
+		if ((tmp = strchr(line, ',')))
+		{
+			*tmp = '\0';
+			tmp++;
+		}
+		if (!*line)
+			record = strdup("-");
+		else
+			record = strdup(line);
+		ft_load_row_data(t, r_idx, record, c_idx);
+		line = tmp;
 		c_idx++;
 	}
 }
